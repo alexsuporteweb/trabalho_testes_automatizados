0a28ba9ad7e86477ffe10756deeb9504ef155e8a
wdsonmedeiro <107139216+wdsonmedeiro@users.noreply.github.com>
2023-12-26 23:11:41 -0400

    Exclusao do Cliente02 conforme combinado

--- src/test/java/ifmt/cba/TesteDeIntegracao/Cliente_EntregadorServico_IntegracaoTest.java
+++ src/test/java/ifmt/cba/TesteDeIntegracao/Cliente_EntregadorServico_IntegracaoTest.java
@@ -1,9 +1,12 @@
 package ifmt.cba.TesteDeIntegracao;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
 
 public class Cliente_EntregadorServico_IntegracaoTest {
 
@@ -28,4 +31,22 @@ public class Cliente_EntregadorServico_IntegracaoTest {
         .then()
             .statusCode(200);
    }
+    @Test
+    public void testeConsultarBuscarEntregadorPorCodigo() {
+        Response response = RestAssured.request(Method.GET, "http://localhost:8080/entregador/codigo/20");
+        Assertions.assertEquals(200, response.getStatusCode());
+    }
+
+   @Test
+    public void testeConsultarBuscarEntregadorPorNome() {
+        Response response = RestAssured.request(Method.GET, "http://localhost:8080/entregador/nome/entregador");
+        Assertions.assertEquals(200, response.getStatusCode());
+
+    }
+    @Test
+    public void testeDeletarEntregadorCodigo() {
+        Response response = RestAssured.request(Method.DELETE, "http://localhost:8080/entregador/2");
+        Assertions.assertEquals(200, response.getStatusCode());
+
+    }  
 }
